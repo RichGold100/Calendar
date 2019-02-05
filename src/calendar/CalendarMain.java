79623c6230286a8dad78a92eb1c1a003c381192c
DESKTOP-4SCGLCJ\kwon <winwin47k@gmail.com>
2019-02-05 19:52:37 +0900

    4.가상달력찍기

--- src/calendar/CalendarMain.java
+++ src/calendar/CalendarMain.java
@@ -40,11 +40,42 @@ public class CalendarMain {
 		if (month>0){
 		int maxDay = maxDays[month-1];		
 		System.out.println(month + "월은 " + maxDay +"일까지 있습니다.");
-		}
 		
-		}
+		
+		if (maxDay == 31){
+		System.out.println("SU MO TU WE TH FR SA");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("29 30 31            ");
+		}else if (maxDay == 30){
+			System.out.println("SU MO TU WE TH FR SA");
+			System.out.println("--------------------");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");
+			System.out.println("29 30               ");				
+		}else if (maxDay == 28){
+			System.out.println("SU MO TU WE TH FR SA");
+			System.out.println("--------------------");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");	
+		
+		  }
+		
+		  }
+		
+		
+		}  //무한루프 종료 -1
+		
 		System.out.println("Have a nice day!");
-		
+	}
+}	
 		
 //		int[] day31 = {1,3,5,7,8,10,12};
 //		int[] day30 = {4,6,9,11};
@@ -77,6 +108,6 @@ public class CalendarMain {
 		
 		
 		
-	}
 
-}
+
+
